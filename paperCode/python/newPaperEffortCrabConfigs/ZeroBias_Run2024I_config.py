374ceda53f9c5dc596f2d9dd18ff5539088de3ca
Andrew Loeliger <loeliger@wisc.edu>
2025-03-05 13:44:56 -0600

    add crab configs to paper code for the new paper effort

---
+++ paperCode/python/newPaperEffortCrabConfigs/ZeroBias_Run2024I_config.py
@@ -0,0 +1,29 @@
+from CRABClient.UserUtilities import config
+import os
+import datetime
+
+today = datetime.date.today().strftime('%d%b%Y')
+cmssw_base = os.getenv('CMSSW_BASE')
+
+config = config()
+
+config.General.requestName = f'AnomalyDetectionPaper2025_ZeroBias_Run2024I_{today}'
+config.General.workArea = './crab'
+config.General.transferOutputs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = f'{cmssw_base}/src/anomalyDetection/paperCode/python/makeCICADANtuplesFromRAW.py'
+config.JobType.pyCfgParams=['isData=True']
+config.JobType.inputFiles=[f'{cmssw_base}/src/anomalyDetection/CICADA']
+config.JobType.maxMemoryMB=4000
+
+config.Data.inputDataset='/ZeroBias/Run2024I-v1/RAW'
+config.Data.inputDBS = 'global'
+config.Data.splitting='FileBased'
+config.Data.unitsPerJob = 2
+#config.Data.runRange='386478,386509,386554,386594,386604,386618,386640,386661,386668,386673,386679,386694,386704,386852,386864,386873,386885,386917,386924,386945,386951'
+config.Data.publication=False
+config.Data.outputDatasetTag = f'AnomalyDetectionPaper2025_ZeroBias_Run2024I_{today}'
+config.Data.outLFNDirBase = '/store/group/phys_exotica/axol1tl/'
+
+config.Site.storageSite = 'T2_CH_CERN'
